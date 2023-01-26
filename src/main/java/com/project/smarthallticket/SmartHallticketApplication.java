a05c3b976920152dbec990c866e7ae843c79d867
RAHUL CHANDRAKANT CHAVAN <rahulcchavan.22@gmail.com>
2023-01-26 11:54:29 +0530

    initial commit

---
+++ src/main/java/com/project/smarthallticket/SmartHallticketApplication.java
@@ -0,0 +1,13 @@
+package com.project.smarthallticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHallticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHallticketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/smarthallticket/SmartHallticketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.smarthallticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHallticketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
